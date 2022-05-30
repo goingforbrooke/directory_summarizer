0c5621cd96d3804ab9216b2260e12061026f013e
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2022-05-30 04:53:47 -0700

    Use borrow more appropriately

--- src/summarize.rs
+++ src/summarize.rs
@@ -6,7 +6,7 @@ use walkdir::WalkDir;
 
 pub fn catalog_directory(target_dir: &PathBuf) -> HashMap<String, i128> {
     let mut filetype_counts = HashMap::<String, i128>::new();
-    let default_extension = &OsString::from("No extension");
+    let default_extension = OsString::from("No extension");
 
     // todo: Check if `test_dir` exists first.
     for entry in WalkDir::new(target_dir)
@@ -14,7 +14,7 @@ pub fn catalog_directory(target_dir: &PathBuf) -> HashMap<String, i128> {
             .into_iter()
             .filter_map(Result::ok)
             .filter(|e| !e.file_type().is_dir()) {
-        let file_ext = entry.path().extension().unwrap_or(default_extension);
+        let file_ext = entry.path().extension().unwrap_or(&default_extension);
         // Add the file path to known file paths with a counter of 0.
         let counter = filetype_counts.entry(String::from(file_ext.to_string_lossy())).or_insert(0);
         // Increment the file path's counter by one.
